8c683517a63f965ed4cd74aa7c4eb42743b8c1da
Chau Nguyen <chaaau.n@googlemail.com>
2015-06-19 16:34:35 +0200

    add code on how to access all elements in a tuple

---
+++ tuple_variadic.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+template <typename T, typename... Tail> struct DoIt;
+
+template <typename T, typename... Tail>
+struct DoIt<std::tuple<T, Tail...>> {
+    void operator() (std::tuple<T, Tail...> & t) {
+        std::cout << std::to_string(std::get<0>(t)) << std::endl;
+    }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<std::size_t> struct len_tuple {};
+
+template <typename Tuple, size_t Id>
+std::string DoSomething(const Tuple & t, len_tuple<Id>) {
+    std::cout << std::get<std::tuple_size<Tuple>::value - Id>(t) << std::endl;
+    return std::get<std::tuple_size<Tuple>::value - Id>(t) + DoSomething(t, len_tuple<Id-1>());
+}
+
+
+template <typename Tuple>
+std::string DoSomething(const Tuple& t, len_tuple<1> /*i am useless*/) {
+    std::cout << std::get<std::tuple_size<Tuple>::value - 1>(t) << std::endl;
+    return std::get<std::tuple_size<Tuple>::value - 1>(t);
+}
+
+template <typename... Args>
+std::string AskForAction(const std::tuple<Args...> & t) {
+    return DoSomething(t, len_tuple<sizeof...(Args)>());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+    auto t = std::make_tuple(1, 2, 3);
+    DoIt<std::tuple<int, int, int>> () (t);
+
+    auto t1 = std::make_tuple( "this ", "is ", "awesomeness" );
+    std::string s = AskForAction(t1);
+
+    std::cout << s << std::endl;
+}
